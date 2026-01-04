31f5f0c0f00417882226a4e345f67bb0e3314667
Abdulla Al Shahariar <abdullaalshahariar@gmail.com>
2026-01-04 14:00:36 +0600

    built the builder and fixed server.js

--- frontend/builder/vite.config.js
+++ frontend/builder/vite.config.js
@@ -5,10 +5,12 @@ import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
   plugins: [devtools(), solidPlugin(), tailwindcss()],
+  base: '/builder/',
   server: {
     port: 3000,
   },
   build: {
     target: 'esnext',
+    outDir: 'dist',
   },
 });
